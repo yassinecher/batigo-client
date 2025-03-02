427c3bf37acc1e7c785f7ef3e0b47cfcf8f6c518
KarimTorjmen4 <karimtorjmen4@gmail.com>
2025-03-02 21:55:41 +0100

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/',
+    apiUrl: 'http://localhost:8081/',
      googleMapsApiKey: 'AIzaSyCYvWCcwoA7KxF-N_-GxHLo8rY5NHjnLKI'
   };
